3bf03fbbb202f659fc2e1cb2d5b7b4723eff8cd5
PhilippGrünig <philipp.gruenig94@gmail.com>
2023-03-03 16:59:45 +0100

    test

--- app/src/main/java/com/example/moviematrix/Home.kt
+++ app/src/main/java/com/example/moviematrix/Home.kt
@@ -15,6 +15,7 @@ private const val ARG_PARAM2 = "param2"
  * A simple [Fragment] subclass.
  * Use the [Home.newInstance] factory method to
  * create an instance of this fragment.
+ ii
  */
 class Home : Fragment() {
     // TODO: Rename and change types of parameters
